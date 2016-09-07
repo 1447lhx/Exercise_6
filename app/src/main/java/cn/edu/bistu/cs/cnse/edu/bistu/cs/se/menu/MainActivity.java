a69e74b814718874b9030a6c52d1b8d02b5bd71f
1447lhx <1959142791@qq.com>
2016-09-07 20:28:41 +0800

    update

--- app/src/main/java/cn/edu/bistu/cs/cnse/edu/bistu/cs/se/menu/MainActivity.java
+++ app/src/main/java/cn/edu/bistu/cs/cnse/edu/bistu/cs/se/menu/MainActivity.java
@@ -1,61 +1,98 @@
 package cn.edu.bistu.cs.cnse.edu.bistu.cs.se.menu;
-
-import android.app.AlertDialog;
- import android.app.Dialog;
- import android.content.DialogInterface;
- import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
  import android.os.Bundle;
- import android.view.ContextMenu;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
  import android.view.LayoutInflater;
  import android.view.Menu;
- import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.view.MenuItem;
  import android.view.View;
  import android.widget.Button;
 import android.widget.EditText;
  import android.widget.PopupMenu;
- import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity
         implements PopupMenu.OnMenuItemClickListener {
-    private Button btn_Context;
-    private Button btn_Pop;
     protected void onCreate(Bundle savedInstanceState) {
-           super.onCreate(savedInstanceState);
-                 setContentView(R.layout.content_main);
-                 btn_Context = (Button) findViewById(R.id.context_menu);
-                 registerForContextMenu(btn_Context);
-                btn_Pop= (Button) findViewById(R.id.pop_menu);
-                btn_Pop.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            PopupMenu popMenu = new PopupMenu(MainActivity.this, view);
-                            popMenu.setOnMenuItemClickListener(MainActivity.this);
-                                 popMenu.inflate(R.menu.menu3);
-                                popMenu.show();
-                             }
-                });}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        TextView textView=(TextView)findViewById(R.id.textview);
+        registerForContextMenu(textView);
+    }
+
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
-                getMenuInflater().inflate(R.menu.menu, menu);
-                return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     public void onCreateContextMenu(ContextMenu menu,View v,ContextMenu.ContextMenuInfo info) {
-               getMenuInflater().inflate(R.menu.menu2, menu);
-               super.onCreateContextMenu(menu,v,info);
+        super.onCreateContextMenu(menu, v, info);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
            }
 
     public boolean onOptionsItemSelected(MenuItem item) {
-        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_settings:
+                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_another:
+                Toast.makeText(this, "close", Toast.LENGTH_SHORT).show();
+                break;
+        }
         return super.onOptionsItemSelected(item);
-           }
+    }
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_another:
+                Toast.makeText(this, "close", Toast.LENGTH_SHORT).show();
+                break;
+        }
         return super.onContextItemSelected(item);
             }
-    @Override
-        public boolean onMenuItemClick(MenuItem menuItem) {
 
-        Toast.makeText(this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
-                 return true;
-            }
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.menu2);
+        popup.show();
+    }
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_another:
+                Toast.makeText(this, "close", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
+    }
      }
